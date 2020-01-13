060ceaaee7460650eccecd619cbe5c816ec3e6b4
Mikkel Ricky <rimi@aarhus.dk>
2020-01-13 14:18:06 +0100

    Clean up options

--- options.js
+++ options.js
@@ -10,10 +10,8 @@ function saveOptions () {
   }, function () {
     // Update status to let user know options were saved.
     var status = document.getElementById('status')
+    status.style.visibility = 'visible'
     status.textContent = 'Options saved.'
-    setTimeout(function () {
-      status.textContent = ''
-    }, 750)
   })
 }
 
