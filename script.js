b0027223eb9027fe6c4313cdc986a4b179c73b41
Mikkel Ricky <rimi@aarhus.dk>
2020-01-13 14:18:17 +0100

    Made stuff work

--- script.js
+++ script.js
@@ -8,19 +8,21 @@ const getInstitutions = () => {
   rows.forEach((row) => {
     const cell = row.querySelector('td:nth-child(2)')
     if (cell !== null) {
-      const match = /^([a-z0-9]{6}) - (.+)/.exec(cell.innerText)
+      const match = new RegExp('^([a-z0-9]{6}) - (.+)', 'i').exec(cell.innerText)
       if (match !== null) {
         const id = match[1]
         const name = match[2]
         const group = row.querySelector('td:nth-child(3)').innerText
         const type = row.querySelector('td:nth-child(4)').innerText
         const numberOfMembers = parseInt(row.querySelector('td:nth-child(5)').innerText)
+        // @TODO Handle duplicate ids!
+
         institutions[id] = {
           id: id,
           name: name,
           group: group,
           type: type,
-          numberOfMembers: numberOfMembers
+          number_of_members: numberOfMembers
         }
       }
     }
@@ -52,15 +54,17 @@ if (button !== null) {
     if (confirm(`Export ${Object.keys(institutions).length} institutions to ${apiUrl}?`)) {
       const xhr = new XMLHttpRequest()
       xhr.open('POST', apiUrl)
-      xhr.setRequestHeader('Authorization', `Token: ${apiToken}`)
-      xhr.send(JSON.stringify(institutions))
+      xhr.setRequestHeader('x-authorization', `token ${apiToken}`)
+      xhr.setRequestHeader('content-type', 'application/vnd.api+json')
+      xhr.send(JSON.stringify({ data: institutions }))
 
       xhr.onreadystatechange = function (event) {
         if (this.readyState === 4) {
-          if (this.status === 201) {
+          if (this.status === 204) {
             alert(`${Object.keys(institutions).length} institutions succesfully exported to ${apiUrl}`)
           } else {
-            alert(`Error exporting ${Object.keys(institutions).length} institutions to ${apiUrl}`)
+            alert(`Error exporting ${Object.keys(institutions).length} institutions to ${apiUrl}:\n${xhr.status} ${xhr.response}`)
+            console.log(xhr)
           }
         }
       }
